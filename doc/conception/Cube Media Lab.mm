f3353c6e8315ce248f10d8cd6d2d2742290bba41
Julien Holtzer <julien@pobot.org>
2015-04-27 15:52:56 +0200

    + ‘doc/conception/Cube Media Lab.mm’

---
+++ doc/conception/Cube Media Lab.mm
@@ -0,0 +1,140 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Cube Media Lab" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1388001731066"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<node TEXT="Enregistrement vid&#xe9;o" POSITION="right" ID="ID_192412577" CREATED="1388001761041" MODIFIED="1388001765675">
+<edge COLOR="#ff00ff"/>
+<node TEXT="depuis une source interne" ID="ID_879538350" CREATED="1388001766337" MODIFIED="1388001772406">
+<node TEXT="cam&#xe9;ra &#xe0; l&apos;int&#xe9;rieur du cube" ID="ID_107874018" CREATED="1388001786048" MODIFIED="1388001792741">
+<node TEXT="webcam USB" ID="ID_587512002" CREATED="1388002129974" MODIFIED="1388002138524"/>
+<node TEXT="cam&#xe9;ra type Raspberry Pi" ID="ID_569611034" CREATED="1388002138765" MODIFIED="1388002144717"/>
+</node>
+</node>
+<node TEXT="depuis une source externe" ID="ID_1654067910" CREATED="1388001772662" MODIFIED="1388001775799">
+<node TEXT="r&#xe9;cup&#xe9;rer un flux wifi" ID="ID_1357102369" CREATED="1388001776991" MODIFIED="1388001783982">
+<node TEXT="depuis une cam&#xe9;ra IP" ID="ID_1714714876" CREATED="1388001807115" MODIFIED="1388001812358"/>
+<node TEXT="depuis la webcam d&apos;un ordinateur" ID="ID_1944219846" CREATED="1388001812614" MODIFIED="1388001830665"/>
+<node TEXT="depuis un logiciel de capture d&apos;&#xe9;cran" ID="ID_868368572" CREATED="1388001817318" MODIFIED="1388001839112"/>
+</node>
+<node TEXT="connexion USB" ID="ID_1434174850" CREATED="1388001797950" MODIFIED="1388001805783">
+<node TEXT="webcam" ID="ID_411128799" CREATED="1388001852918" MODIFIED="1388001856226">
+<node TEXT="une seule &#xe0; la fois" ID="ID_482946534" CREATED="1388002107385" MODIFIED="1388002122081"/>
+<node TEXT="plusieurs en parall&#xe8;le" ID="ID_651535218" CREATED="1388002115162" MODIFIED="1388002127261"/>
+</node>
+<node TEXT="cl&#xe9; USB" ID="ID_567302152" CREATED="1388001856654" MODIFIED="1388001867637">
+<node TEXT="recherche de toutes les vid&#xe9;os pr&#xe9;sentes" ID="ID_1092931864" CREATED="1388001868252" MODIFIED="1388001875492"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Sauvegarde" POSITION="right" ID="ID_1796383442" CREATED="1388001977999" MODIFIED="1388001981697">
+<edge COLOR="#ffff00"/>
+<node TEXT="interne" ID="ID_138496333" CREATED="1388001982140" MODIFIED="1388001992196">
+<node TEXT="dans la m&#xe9;moire du cube" ID="ID_999880154" CREATED="1388001995837" MODIFIED="1388002000565"/>
+</node>
+<node TEXT="externe" ID="ID_983129579" CREATED="1388001992530" MODIFIED="1388001993888">
+<node TEXT="sur un p&#xe9;riph&#xe9;rique (cl&#xe9; USB)" ID="ID_1217776876" CREATED="1388002003676" MODIFIED="1388002012944"/>
+<node TEXT="sur un espace disque du r&#xe9;seau wifi" ID="ID_452399933" CREATED="1388002009004" MODIFIED="1388002029675"/>
+<node TEXT="sur le Net" ID="ID_182077200" CREATED="1388002029931" MODIFIED="1388002031835">
+<node TEXT="upload Vimeo automatis&#xe9;" ID="ID_959862520" CREATED="1388002032388" MODIFIED="1388002036367"/>
+<node TEXT="upload Dropbox" ID="ID_1481016400" CREATED="1388002036639" MODIFIED="1388002041788"/>
+<node TEXT="enregistrement direct des flux ?" ID="ID_1395449107" CREATED="1388002043199" MODIFIED="1388002061163"/>
+</node>
+</node>
+</node>
+<node TEXT="Afficheur" POSITION="right" ID="ID_1571398773" CREATED="1388001896403" MODIFIED="1388001898588">
+<edge COLOR="#00ffff"/>
+<node TEXT="Par leds color&#xe9;es" ID="ID_174265836" CREATED="1388001899031" MODIFIED="1388001902839">
+<node TEXT="Rouge, vert, bleu, orange, ..." ID="ID_119643273" CREATED="1388001931081" MODIFIED="1388001940520"/>
+<node TEXT="Associ&#xe9;es &#xe0; un logo" ID="ID_741188455" CREATED="1388001940995" MODIFIED="1388001947080">
+<node TEXT="Wifi connect&#xe9; / perdu / connexion au serveur" ID="ID_1729297730" CREATED="1388001947586" MODIFIED="1388001972696"/>
+<node TEXT="Enregistrement en cours" ID="ID_1714730375" CREATED="1388001948780" MODIFIED="1388001958359"/>
+<node TEXT="Probl&#xe8;me d&apos;espace disque" ID="ID_797783845" CREATED="1388001958678" MODIFIED="1388001962844"/>
+<node TEXT="Attente d&apos;une connexion (identifi&#xe9;e par USB)" ID="ID_1761428899" CREATED="1388002326656" MODIFIED="1388002335066"/>
+</node>
+</node>
+<node TEXT="Ecran textuel" ID="ID_1156272368" CREATED="1388001903095" MODIFIED="1388001905342">
+<node TEXT="LCD 2 lignes 16 colonnes" ID="ID_1998723977" CREATED="1388001905802" MODIFIED="1388001928048"/>
+<node TEXT="LCD graphique" ID="ID_1259667392" CREATED="1388001919881" MODIFIED="1388001925030"/>
+<node TEXT="OLED" ID="ID_870403723" CREATED="1388001917907" MODIFIED="1388001919359"/>
+</node>
+<node TEXT="Par console s&#xe9;rie" ID="ID_1587465702" CREATED="1388049245908" MODIFIED="1388049251855"/>
+</node>
+<node TEXT="Interactions" POSITION="right" ID="ID_904337921" CREATED="1388002160207" MODIFIED="1388002163437">
+<edge COLOR="#7c0000"/>
+<node TEXT="Boutons" ID="ID_731202162" CREATED="1388002164006" MODIFIED="1388002191112">
+<node TEXT="Croix directionnelle" ID="ID_1343577491" CREATED="1388002169208" MODIFIED="1388002175636"/>
+<node TEXT="Associ&#xe9;es &#xe0; un logo" ID="ID_65260608" CREATED="1388002175893" MODIFIED="1388002179653"/>
+</node>
+<node TEXT="Interrupteurs" ID="ID_1099223788" CREATED="1388002182913" MODIFIED="1388002185769">
+<node TEXT="Marche/arr&#xea;t" ID="ID_833094241" CREATED="1388002340509" MODIFIED="1388002347249"/>
+</node>
+<node TEXT="Lecteur RFID" ID="ID_1312631699" CREATED="1388002195647" MODIFIED="1388002203152">
+<node TEXT="Pour automatiser les t&#xe2;ches" ID="ID_67856458" CREATED="1388002301033" MODIFIED="1388002307180"/>
+<node TEXT="Pour reconnaitre les p&#xe9;riph&#xe9;riques et leur r&#xf4;le" ID="ID_469660186" CREATED="1388002308716" MODIFIED="1388002318467"/>
+<node TEXT="Pour autoriser les utilisateurs" ID="ID_605770967" CREATED="1388049005829" MODIFIED="1388049009692"/>
+</node>
+<node TEXT="Reconnaissance vocale" ID="ID_1586724950" CREATED="1388002205319" MODIFIED="1388002225093">
+<node TEXT="N&#xe9;cessite un microphone" ID="ID_1468426996" CREATED="1388002225568" MODIFIED="1388002231801"/>
+<node TEXT="Voir S.A.R.A.H." ID="ID_1488298143" CREATED="1388002289224" MODIFIED="1388002299271"/>
+</node>
+<node TEXT="Synth&#xe8;se vocale" ID="ID_1280348070" CREATED="1388048971951" MODIFIED="1388048979003">
+<node TEXT="Messages d&apos;alerte" ID="ID_834784215" CREATED="1388048975310" MODIFIED="1388048983086">
+<node TEXT="Espace disque indisponible" ID="ID_1508608362" CREATED="1388048983439" MODIFIED="1388048986463"/>
+<node TEXT="Enregistrement interrompu" ID="ID_122670249" CREATED="1388048986811" MODIFIED="1388048991030"/>
+<node TEXT="Perte de connexion r&#xe9;seau" ID="ID_1475741565" CREATED="1388048991380" MODIFIED="1388048997091"/>
+</node>
+</node>
+<node TEXT="Par interface r&#xe9;seau" ID="ID_113759992" CREATED="1388049254886" MODIFIED="1388049257681"/>
+<node TEXT="Par console s&#xe9;rie" ID="ID_896794863" CREATED="1388049257885" MODIFIED="1388049260780"/>
+</node>
+</node>
+</map>
