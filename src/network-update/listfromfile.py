2587d7369a934e1b9db633679ab4770778e67779
makoman <nukman.naim@gmail.com>
2015-05-11 06:49:34 +0200

    Traduction
    + network-update

--- src/network-update/listfromfile.py
+++ src/network-update/listfromfile.py
@@ -1,22 +1,22 @@
 #open file to read
 f = open('test', "r")
-#open file to write		
-w = open('listssid', "w")	
+#open file to write
+w = open('listssidfromfile', "w")
 
 for line in iter(f):
-	
+
 	target = "ssid"
 	words = line.split("=")
 	prevline = ""
-	
+
 	#if previous line is ssid, take current line
 	for line in words:
-		if 'ssid' in prevline:	
+		if 'ssid' in prevline:
 			ssid = line[1:-1].rstrip('"')
 			print ssid
 			w.write(ssid + "\n")
 		prevline = line
-	
-			
+
+
 w.close()
 f.close()
