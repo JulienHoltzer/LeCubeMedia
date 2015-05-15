c505c69718ac93f703278afc2a70ef64291226cd
Julien Holtzer <julien@pobot.org>
2015-05-15 12:47:27 +0200

    update twitter module

--- src/lecube/mod/test.py
+++ src/lecube/mod/test.py
@@ -6,10 +6,33 @@ import time
 import datetime
 
 
-def handle_project_tag(ttype, data):
-	logging.info("Jadi woh benda ni tengok : %s !" %data)
+class TestManager :
+
+    def __init__(self, cube, tagtype):
+	self.cube = cube
+	self.recordv = None
+	self.recorda = None
+	cube.register_tag_handler(tagtype,self.handle_action_tag)
+
+    def handle_action_tag(self, ttype, data):
+	logging.debug("Test : %s",data)
+	
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+	
+	# TODO : pas de chemin en dur	
+	# TODO : creer un repertoire par PROJET (par exemple)
+	filename = '/home/pi/images/'+ st +'.jpg'
+	command = 'raspistill -hf -vf --width 1280 --height 720 --timeout 1000 -awb auto -o '+filename
+	logging.debug(command)
+	subprocess.call(command.split(),shell=False)
+	
+	#test to tweet string
+	say = "CubeCubicCubicleTest"
+	self.cube.tag_detection('TWITTER','TWITTER:'+filename)
+	logging.debug('CLIC !')
 
 def init(cube, params):
-	logging.info("Mulakan process")
-	tagtype = params.get("tagtype","prj")
-	cube.register_tag_handler(tagtype, handle_project_tag)
+	logging.info("Initialisation du module : TEST")
+	tagtype = params.get("tagtype","tst")
+	cube.test_manager = TestManager(cube, tagtype)
--- src/lecube/mod/twitter.py
+++ src/lecube/mod/twitter.py
@@ -1,22 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import subprocess
+import time
+import datetime
 import tweepy
+
+class TwitterManager :
+	
+
+
+    def __init__(self, cube, tagtype):
+	self.cube = cube
+	self.recordv = None
+	self.recorda = None
+	cube.register_tag_handler(tagtype,self.handle_action_tag)
+
+    def handle_action_tag(self, ttype, data):
+	logging.debug("Test : %s",data)
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
+
+	#TWITTER API
+	cfg = {
+		"CONSUMER_KEY": "value",
+		"CONSUMER_SECRET": "valuee",
+		"OAUTH_TOKEN": "valueee",
+		"OAUTH_TOKEN_SECRET": "valueeee"
+	}
+
+	api = get_api(cfg)
+	photo_path = data
+	tweet = "SoFAB: " + st
+	status = api.update_with_media(data, status=tweet)
+	logging.debug('SECOND CLIC !')
 
 def get_api(cfg):
 	auth = tweepy.OAuthHandler(cfg['CONSUMER_KEY'], cfg['CONSUMER_SECRET'])
 	auth.set_access_token(cfg['OAUTH_TOKEN'], cfg['OAUTH_TOKEN_SECRET'])
 	return tweepy.API(auth)
 
-def main():
-
-	cfg = {
-		"CONSUMER_KEY": "IUVGYfSZtIns5ZxF0RLiIsNbZ",
-		"CONSUMER_SECRET": "XHdwHAFGAmnP3XfYDrIilrOu92rQbBhbs5psS2goW8Ewed2nxI",
-		"OAUTH_TOKEN": "462977723-bJaDN93XW9XVPzNbTQjOjNwEnlZPcJhWYTX20SKr",
-		"OAUTH_TOKEN_SECRET": "XGVISj0TvUWFnYnmZQfW9zFJj3ADyGZkKDB95b4H0pPbI"
-	}
-
-	api = get_api(cfg)
-	tweet = "TESTTEST Hello, world!"
-	status = api.update_status(status=tweet)
-
-if __name__ == "__main__":
-	main()
+def init(cube, params):
+	logging.info("Project Twitter")
+	tagtype = params.get("tagtype","twt")
+	cube.twitter_manager = TwitterManager(cube, tagtype)
